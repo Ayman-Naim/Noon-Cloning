043c5e9153a957324de472874bb0beae7e935539
Ayman-Naim <mnameno_2012@yahoo.com>
2024-04-27 23:27:52 +0300

    Initial Commit

---
+++ NoonCloning/NoonCloningApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoonCloningApp.swift
+//  NoonCloning
+//
+//  Created by ayman on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct NoonCloningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
